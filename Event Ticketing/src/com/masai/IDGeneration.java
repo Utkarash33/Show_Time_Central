7ca0964adbe021d794459e15052f119d75bdc6e4
atulthakur3303@gmail.com <atulthakur3303@gmail.com>
2023-06-24 17:44:17 +0530

    Upgrade the Event Id generator class

--- Event Ticketing/src/com/masai/IDGeneration.java
+++ Event Ticketing/src/com/masai/IDGeneration.java
@@ -1,10 +1,34 @@
 package com.masai;
 
+import java.util.Map;
+
+import com.masai.event.Event;
+
 public class IDGeneration {
 
 	public static int generateId() {
+		Map<Integer, Event> event = FileExists.eventFile();
 		
-		return (int) (Math.random() * 100000);
+		
+		
+		int id = (int) (Math.random() * 100000);
+		boolean flag =true;
+		for (Map.Entry<Integer, Event> me : event.entrySet()) {
+			   Event a =me.getValue();
+			   
+			   if(a.getId()==id)
+			   {   
+				flag= false;
+			   }   
+		}
+		if(flag)
+		{
+			return  (int) (Math.random() * 100000);
+		}
+		else
+		{
+			return id;
+		}
 	}
 
 }
